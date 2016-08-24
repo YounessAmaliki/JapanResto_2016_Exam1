aa73e449bc7333160f62af9ca4e2be84519dd51e
Youness Amaliki <s061828@ap.be>
2016-08-24 16:16:30 +0200

    Almost done
    Fixing update datestring

--- app/_attachments/scripts/couch.js
+++ app/_attachments/scripts/couch.js
@@ -14,13 +14,14 @@ function createDoc(){
 	doc.tablenumber = parseInt(tablenumber);
 	doc.remarks = remarks;
 	doc.datestring = datestring;
+	doc.flag = "Not Done";
 	var json = JSON.stringify(doc);
 		
 	console.log(json);
 	
 	$.ajax({
 		type:	'PUT',
-		url:	'../../' + meal + datestring,
+		url:	'../../' + meal + " " + datestring,
 		data:	json,
 		contentType: 'application/json',
 		async:	true,
@@ -47,7 +48,7 @@ function buildOutput(){
         		
         		if(arr[i].id.indexOf('_design') == -1){
         			var doc = arr[i].doc;
-        			html += '<tr><td>' + doc.meal + '</td><td>' + doc.amount + '</td><td>' + doc.tablenumber + '</td><td>'+ doc.remarks + '</td><td>' + doc.datestring + '</td></tr>';
+        			html += '<tr><td>' + doc.meal + '</td><td>' + doc.amount + '</td><td>' + doc.tablenumber + '</td><td>'+ doc.remarks + '</td><td>' + doc.datestring + '</td><td>' + doc.flag + '</td>' + '<td><button type="button" class="btn btn-success" onClick="editDoc(\'' + doc._id + '\',\'' + doc._rev + '\',\''  + doc.amount + '\',\'' + doc.tablenumber + '\',\'' + doc.remarks + '\',\'' + doc.datestring + '\',\'' + doc.flag +  '\')">Edit</button></td></tr>';;
         		}
         	}
         	html += '</table>';
@@ -56,6 +57,68 @@ function buildOutput(){
         },
 		error: function(XMLHttpRequest, textStatus, errorThrown){ 
 			console.log(errorThrown); 
+		}
+	});
+}
+
+
+function editDoc(id, rev, meal, amount, tablenumber, remarks, datestring, flag){
+	
+	$('#output').hide();
+	$('#edit').show();
+	
+	var html = '';
+	
+	// Build edit form
+	html += '<h3>Edit?</h3><table class="table table-hover">';
+	html += '<input type="hidden" id="_id" value="' + id + '"/>';
+	html += '<input type="hidden" id="_rev" value="' + rev + '"/>';
+	html += '<tr><td>Opgeleverd ?</td><td><select id="flag"><option value="1">Done</option><option value="1">Not Done</option></td></tr>';
+	html += '<tr><td colspan="2" align="center"><button type="button" class="btn btn-primary" onClick="updateDoc()">Ok</button></td></tr>';
+	
+	$('#edit').html(html);
+}
+
+
+function updateDoc(datestring){
+	
+	var id = $("#_id").val();
+	var rev = $("#_rev").val();
+	var flag = $("#flag :selected").text();
+	var meal = $("#meal :selected").text();
+	var amount = $("#amount").val();
+	var tablenumber = $("#tablenumber").val();
+	var remarks = $("#remarks").val();
+	
+
+
+
+
+	var doc = {};
+
+	doc._id = id;
+	doc._rev = rev;
+	doc.flag = flag;
+	doc.meal = meal;
+	doc.amount = amount;
+	doc.tablenumber = tablenumber;
+	doc.remarks = remarks;
+	doc.datestring = datestring;
+	var json = JSON.stringify(doc);
+
+	$.ajax({
+		type : 'PUT',
+		url : '../../' + id,
+		data : json,
+		contentType : 'application/json',
+		async : true,
+		success : function(data){
+			$('#edit').hide();
+			$('#output').show();
+			buildOutput();
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown){
+			console.log(errorThrown);
 		}
 	});
 }
